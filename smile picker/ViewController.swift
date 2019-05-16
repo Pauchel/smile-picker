7c3e9c1110aaf8c710ef610bf3fab101115e0b84
山田 怜音iPAD <ameamerain0119@icloud.com>
2019-05-16 18:27:06 +0900

    Initial Commit

---
+++ smile picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smile picker
+//
+//  Created by 山田　怜音 on 2019/05/16.
+//  Copyright © 2019 山田　怜音. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
